e9e816f3a19168c450678e84ba99ad69bad38beb
Kartik Nayak <thisiskartiknayak@gmail.com>
2024-02-14 11:52:50 +0530

    Update: App version updated

--- lib/features/settings/widgets/app_version_card.dart
+++ lib/features/settings/widgets/app_version_card.dart
@@ -23,7 +23,7 @@ class AppVersionCard extends StatelessWidget {
           ),
           const SizedBox(width: 8),
           Text(
-            'Historian v1.1.0',
+            'Historian v1.3.2',
             style: Theme.of(context).textTheme.titleSmall!.copyWith(
                   fontWeight: FontWeight.w300,
                   letterSpacing: 0.5,
