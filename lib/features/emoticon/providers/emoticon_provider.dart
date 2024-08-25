f50768c6269f134f4c943b8e93d9f6a3b878a3df
Kartik Nayak <thisiskartiknayak@gmail.com>
2024-08-26 01:17:28 +0530

    feat: add EmoticonProvider for EmoticonPage state management

---
+++ lib/features/emoticon/providers/emoticon_provider.dart
@@ -0,0 +1,89 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+import '../models/emoticon.dart';
+
+class EmoticonProvider extends ChangeNotifier {
+  // getters
+  static List<(String, Iterable<Emoticon>)> get categories => _categories;
+  Iterable<Emoticon> get searchResultList => _searchResultList;
+
+  TabController get tabController => _tabController;
+  ScrollController get scrollController => _scrollController;
+  TextEditingController get searchController => _searchController;
+
+  int get currentTabIndex => _tabController.index;
+  bool get showSearchBar => _showSearchBar;
+  bool get showRecent => _showRecent;
+
+  // private var
+  static late final List<(String, Iterable<Emoticon>)> _categories;
+  late Iterable<Emoticon> _searchResultList;
+
+  late final TabController _tabController;
+  late final ScrollController _scrollController;
+  late final TextEditingController _searchController;
+
+  bool _showSearchBar = false;
+  bool _showRecent = false;
+  int _hoveredItemIndex = -1;
+  bool _isInitialized = false;
+
+  // public methods
+  void initControllers(TickerProvider vsync) {
+    if (_isInitialized) return;
+
+    debugPrint('HomeProvider initControllers is called');
+    _initEmoticonCategories();
+    _searchResultList = [];
+    _tabController = TabController(length: 3, vsync: vsync);
+    _tabController.index = 2;
+    _scrollController = ScrollController();
+    _searchController = TextEditingController();
+    _isInitialized = true;
+  }
+
+  @override
+  void dispose() {
+    _tabController.dispose();
+    _searchController.dispose();
+    super.dispose();
+  }
+
+  void updateTabIndex(int index) {
+    _tabController.index = index;
+
+    _showSearchBar = currentTabIndex == 0;
+    _showRecent = currentTabIndex == 1;
+
+    if (currentTabIndex == 2) _searchController.clear();
+
+    notifyListeners();
+  }
+
+  bool isHovering(int index) => _hoveredItemIndex == index;
+
+  void updateHoverState(bool isHovering, int index) {
+    _hoveredItemIndex = isHovering ? index : -1;
+    notifyListeners();
+  }
+
+  void copyEmoticon(String emoticon) =>
+      Clipboard.setData(ClipboardData(text: emoticon));
+
+  void searchEmoticon(String query) {
+    _searchResultList = Emoticons.all().where(
+      (emoticon) => emoticon.name.toLowerCase().contains(query.toLowerCase()),
+    );
+    _scrollController.jumpTo(0);
+
+    notifyListeners();
+  }
+
+  // private methods
+  void _initEmoticonCategories() {
+    _categories = [
+      ('Emoticons', Emoticons.all()),
+    ];
+  }
+}
