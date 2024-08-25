7d2ab271d34912c27bbf052d826e8081c534bc6a
Kartik Nayak <thisiskartiknayak@gmail.com>
2024-08-25 17:01:31 +0530

    feat: add pages for TabBar in HomePage

---
+++ lib/features/emoji/pages/emoji_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class EmojiPage extends StatelessWidget {
+  const EmojiPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text('EmojiPage'),
+      ),
+    );
+  }
+}
---
+++ lib/features/emoticon/pages/emoticon_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class EmoticonPage extends StatelessWidget {
+  const EmoticonPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text('EmoticonPage'),
+      ),
+    );
+  }
+}
