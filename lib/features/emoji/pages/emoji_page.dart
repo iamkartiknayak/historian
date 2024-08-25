008774aff90687ebe08695b39482f94bc05cd8ef
Kartik Nayak <thisiskartiknayak@gmail.com>
2024-08-25 21:11:59 +0530

    feat: enhance EmojiPage with state management and custom widgets

--- lib/features/emoji/pages/emoji_page.dart
+++ lib/features/emoji/pages/emoji_page.dart
@@ -1,13 +1,52 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
-class EmojiPage extends StatelessWidget {
+import '../../../common/custom_tab_bar.dart';
+import '../../../common/custom_tab_item.dart';
+import '../providers/emoji_provider.dart';
+import '../widgets/build_emojis.dart';
+import '../widgets/build_recent_emojis.dart';
+
+class EmojiPage extends StatefulWidget {
   const EmojiPage({super.key});
 
   @override
+  State<EmojiPage> createState() => _EmojiPageState();
+}
+
+class _EmojiPageState extends State<EmojiPage> with TickerProviderStateMixin {
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('EmojiPage'),
+    debugPrint('EmojiPage build is called');
+    final emojiProvider = context.read<EmojiProvider>();
+    emojiProvider.initController(this);
+    final showRecent = context.select((EmojiProvider p) => p.showRecent);
+
+    return Scaffold(
+      appBar: AppBar(
+        toolbarHeight: 0.0,
+        bottom: PreferredSize(
+          preferredSize: const Size.fromHeight(50.0),
+          child: CustomTabBar(
+              onTap: emojiProvider.updateTabIndex,
+              tabController: emojiProvider.tabController,
+              tabs: const [
+                CustomTabItem(iconPath: 'assets/svgs/search.svg'),
+                CustomTabItem(iconPath: 'assets/svgs/recent.svg'),
+                CustomTabItem(iconPath: 'assets/svgs/emoji_outlined.svg'),
+                CustomTabItem(iconPath: 'assets/svgs/animals_nature.svg'),
+                CustomTabItem(iconPath: 'assets/svgs/food_drink.svg'),
+                CustomTabItem(iconPath: 'assets/svgs/activity.svg'),
+                CustomTabItem(iconPath: 'assets/svgs/travel_places.svg'),
+                CustomTabItem(iconPath: 'assets/svgs/objects.svg'),
+                CustomTabItem(iconPath: 'assets/svgs/symbols.svg'),
+                CustomTabItem(iconPath: 'assets/svgs/flags.svg'),
+              ]),
+        ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 12.0),
+        child: showRecent ? const BuildRecentEmojis() : const BuildEmojis(),
       ),
     );
   }
