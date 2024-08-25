35dfb4f0ef075e9af708c885bff4284178279a91
Kartik Nayak <thisiskartiknayak@gmail.com>
2024-08-25 20:59:27 +0530

    feat: integrate EmojiProvider into MultiProvider in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
+import './features/emoji/providers/emoji_provider.dart';
 import './features/home/pages/home_page.dart';
 import './features/home/providers/home_provider.dart';
 
 void main() => runApp(MultiProvider(
       providers: [
         ChangeNotifierProvider(create: (_) => HomeProvider()),
+        ChangeNotifierProvider(create: (_) => EmojiProvider()),
       ],
       child: const Historian(),
     ));
