9d02cbecb72391c38db6de49454df00cc52d8bde
Kartik Nayak <thisiskartiknayak@protonmail.com>
2024-09-15 12:16:28 +0530

    feat: implement navigation to SettingsPage on button tap

--- lib/features/home/widgets/settings_button.dart
+++ lib/features/home/widgets/settings_button.dart
@@ -1,13 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 
+import '../../settings/pages/settings_page.dart';
+
 class SettingsButton extends StatelessWidget {
   const SettingsButton({super.key});
 
   @override
   Widget build(BuildContext context) {
     return InkWell(
-      onTap: () {}, // TODO: Implement navigation to SettingsPage
+      onTap: () => Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (context) => const SettingsPage(),
+        ),
+      ),
       child: SvgPicture.asset(
         'assets/svgs/settings_outlined.svg',
         colorFilter: ColorFilter.mode(
