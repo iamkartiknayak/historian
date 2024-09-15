74aa237841f81c3761e02924c59c48a1d5bc08bc
Kartik Nayak <thisiskartiknayak@protonmail.com>
2024-09-15 12:36:53 +0530

    feat: refactor main.dart to include SettingsProvider and initialize controllers

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,7 @@ import './features/emoji/providers/emoji_provider.dart';
 import './features/emoticon/providers/emoticon_provider.dart';
 import './features/home/pages/home_page.dart';
 import './features/home/providers/home_provider.dart';
+import './features/settings/providers/settings_provider.dart';
 
 void main() => runApp(MultiProvider(
       providers: [
@@ -13,15 +14,27 @@ void main() => runApp(MultiProvider(
         ChangeNotifierProvider(create: (_) => ClipboardProvider()),
         ChangeNotifierProvider(create: (_) => EmojiProvider()),
         ChangeNotifierProvider(create: (_) => EmoticonProvider()),
+        ChangeNotifierProvider(create: (_) => SettingsProvider()),
       ],
       child: const Historian(),
     ));
 
-class Historian extends StatelessWidget {
+class Historian extends StatefulWidget {
   const Historian({super.key});
 
   @override
+  State<Historian> createState() => _HistorianState();
+}
+
+class _HistorianState extends State<Historian> with TickerProviderStateMixin {
+  @override
   Widget build(BuildContext context) {
+    context.read<HomeProvider>().initControllers(this);
+    context.read<ClipboardProvider>().initControllers();
+    context.read<EmojiProvider>().initControllers(this);
+    context.read<EmoticonProvider>().initControllers(this);
+    context.read<SettingsProvider>().initControllers(this);
+
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Historian',
