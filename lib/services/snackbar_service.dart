73eb11a853f8a396c9dcc59e7a485db781571fa5
Kartik Nayak <thisiskartiknayak@protonmail.com>
2024-09-16 11:05:20 +0530

    feat: implement snackbar

---
+++ lib/services/snackbar_service.dart
@@ -0,0 +1,84 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../features/clipboard/providers/clipboard_provider.dart';
+import '../features/home/pages/home_page.dart';
+
+class SnackBarService {
+  static final scaffoldKey = GlobalKey<ScaffoldMessengerState>();
+
+  static void showSnackBar({
+    required BuildContext context,
+    required String message,
+    bool showUndo = false,
+    int time = 200,
+  }) {
+    final clipboardProvider = context.read<ClipboardProvider>();
+
+    scaffoldKey.currentState
+        ?.showSnackBar(
+          SnackBar(
+            duration: Duration(milliseconds: showUndo ? 5000 : time),
+            content: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  message,
+                  style: Theme.of(context).textTheme.labelMedium!.copyWith(
+                        color: Colors.black,
+                      ),
+                ),
+                if (showUndo)
+                  _UndoButton(
+                    clipboardProvider: clipboardProvider,
+                    scaffoldKey: scaffoldKey,
+                  ),
+              ],
+            ),
+            backgroundColor: Colors.teal,
+          ),
+        )
+        .closed
+        .then((SnackBarClosedReason reason) {
+      final homePageState = homePageKey.currentState;
+      final tabIndex = homePageState?.homeTabIndex;
+
+      if (tabIndex != 0) return;
+
+      switch (reason) {
+        case SnackBarClosedReason.hide:
+          break;
+
+        default:
+          if (showUndo) clipboardProvider.deleteItemPermanently();
+          break;
+      }
+    });
+  }
+}
+
+class _UndoButton extends StatelessWidget {
+  const _UndoButton({
+    required this.clipboardProvider,
+    required this.scaffoldKey,
+  });
+
+  final ClipboardProvider clipboardProvider;
+  final GlobalKey<ScaffoldMessengerState> scaffoldKey;
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTapUp: (tap) {
+        clipboardProvider.undoDeleteItem();
+        scaffoldKey.currentState?.hideCurrentSnackBar();
+      },
+      child: Text(
+        'Undo',
+        style: Theme.of(context).textTheme.labelMedium!.copyWith(
+              color: Colors.black,
+            ),
+      ),
+    );
+  }
+}
