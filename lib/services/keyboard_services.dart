14173a83dcc68e4c2af2fca70c025daaf018e196
Kartik Nayak <thisiskartiknayak@protonmail.com>
2024-09-16 19:05:51 +0530

    feat: implemented keyboard shortcuts

---
+++ lib/services/keyboard_services.dart
@@ -0,0 +1,128 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:provider/provider.dart';
+
+import './snackbar_service.dart';
+import '../features/clipboard/providers/clipboard_provider.dart';
+import '../features/emoji/providers/emoji_provider.dart';
+import '../features/emoticon/providers/emoticon_provider.dart';
+import '../features/home/pages/home_page.dart';
+import '../features/home/providers/home_provider.dart';
+
+class KeyboardService {
+  static late BuildContext _context;
+  static final Set<LogicalKeyboardKey> _pressedKeys = {};
+  static bool _isKeyboardInitialized = false;
+
+  static void initKeyboardListener(BuildContext context) {
+    _context = context;
+    if (_isKeyboardInitialized) return;
+
+    debugPrint('KeyboardListener is initialized');
+    HardwareKeyboard.instance.addHandler(_handleKeyPress);
+    _isKeyboardInitialized = true;
+  }
+
+  static void dispose() =>
+      HardwareKeyboard.instance.removeHandler(_handleKeyPress);
+
+  static bool _handleKeyPress(KeyEvent event) {
+    final homePageState = homePageKey.currentState;
+    final tabIndex = homePageState?.homeTabIndex;
+
+    final homeProvider = _context.read<HomeProvider>();
+    final clipboardProvider = _context.read<ClipboardProvider>();
+    final emojiProvider = _context.read<EmojiProvider>();
+    final emoticonProvider = _context.read<EmoticonProvider>();
+
+    if (event is KeyDownEvent) {
+      _pressedKeys.add(event.logicalKey);
+
+      if (_pressedKeys.contains(LogicalKeyboardKey.controlLeft)) {
+        switch (event.logicalKey) {
+          case LogicalKeyboardKey.digit1:
+            homeProvider.updateTabIndex(0);
+            return false;
+
+          case LogicalKeyboardKey.digit2:
+            homeProvider.updateTabIndex(1);
+            return false;
+
+          case LogicalKeyboardKey.digit3:
+            homeProvider.updateTabIndex(2);
+            return false;
+        }
+      }
+
+      debugPrint('CTI : $tabIndex');
+
+      switch (tabIndex) {
+        case 0:
+          _handleClipboardPage(event, clipboardProvider);
+          return false;
+
+        case 1:
+          _handleEmojiPage(emojiProvider);
+          break;
+
+        case 2:
+          _handleEmoticonPage(emoticonProvider);
+          break;
+      }
+    } else if (event is KeyUpEvent) {
+      _pressedKeys.remove(event.logicalKey);
+    }
+    return false;
+  }
+
+  static void _handleClipboardPage(KeyEvent event, ClipboardProvider provider) {
+    final activeItemIndex = provider.activeItemIndex;
+    if (_pressedKeys.contains(LogicalKeyboardKey.arrowUp)) {
+      final isLowerBound = activeItemIndex <= 0;
+      provider.setActiveItemIndex(isLowerBound ? 0 : -1);
+      provider.keyboardNavScroll(false);
+    } else if (_pressedKeys.contains(LogicalKeyboardKey.arrowDown)) {
+      final isUpperBound = activeItemIndex >= provider.clipboard.length - 1;
+      provider.setActiveItemIndex(isUpperBound ? 0 : 1);
+      provider.keyboardNavScroll(true);
+      debugPrint('Arrow down: Index 0');
+    } else if (_pressedKeys.contains(LogicalKeyboardKey.controlLeft)) {
+      switch (event.logicalKey) {
+        case LogicalKeyboardKey.keyS:
+          debugPrint('Save if image: Index \$activeItemIndex');
+          break;
+
+        case LogicalKeyboardKey.keyD:
+          provider.deleteItem(provider.activeItemIndex);
+          break;
+
+        case LogicalKeyboardKey.keyC:
+          provider.copyItem(provider.activeItemIndex);
+          break;
+
+        case LogicalKeyboardKey.keyP:
+          provider.handleItemPin(provider.activeItemIndex);
+          break;
+
+        case LogicalKeyboardKey.keyU:
+          provider.undoDeleteItem();
+          SnackBarService.scaffoldKey.currentState?.hideCurrentSnackBar();
+          break;
+      }
+    }
+  }
+
+  static void _handleEmojiPage(EmojiProvider provider) {
+    if (_pressedKeys.contains(LogicalKeyboardKey.controlLeft) &&
+        _pressedKeys.contains(LogicalKeyboardKey.keyS)) {
+      provider.toggleSearchBar();
+    }
+  }
+
+  static void _handleEmoticonPage(EmoticonProvider provider) {
+    if (_pressedKeys.contains(LogicalKeyboardKey.controlLeft) &&
+        _pressedKeys.contains(LogicalKeyboardKey.keyS)) {
+      provider.toggleSearchBar();
+    }
+  }
+}
