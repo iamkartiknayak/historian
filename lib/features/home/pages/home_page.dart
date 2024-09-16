8e3b7445697124d4193663f8078287a1bbe11256
Kartik Nayak <thisiskartiknayak@protonmail.com>
2024-09-16 19:18:53 +0530

    feat: add keyboard service init and minor refactor

--- lib/features/home/pages/home_page.dart
+++ lib/features/home/pages/home_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
+import '../../../services/keyboard_services.dart';
 import '../../clipboard/pages/clipboard_page.dart';
 import '../../emoji/pages/emoji_page.dart';
 import '../../emoticon/pages/emoticon_page.dart';
@@ -8,7 +9,7 @@ import '../providers/home_provider.dart';
 import '../widgets/home_tab_bar.dart';
 import '../widgets/settings_button.dart';
 
-final GlobalKey<HomePageState> homePageKey = GlobalKey<HomePageState>();
+final homePageKey = GlobalKey<HomePageState>();
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -21,8 +22,15 @@ class HomePageState extends State<HomePage> {
   int homeTabIndex = 0;
 
   @override
+  void dispose() {
+    KeyboardService.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
     debugPrint('HomePage build is called');
+    KeyboardService.initKeyboardListener(context);
     homeTabIndex = context.select((HomeProvider p) => p.currentTabIndex);
 
     return Scaffold(
